e3de4e21f0c9553f67d49c62e948f129d4de4093
llarreta <leonel@larreta.com.ar>
2020-05-04 19:01:10 -0300

    En caso de que no sea necesario autorizar no sera necesario instanciar el JWT processor

--- src/main/java/ar/com/intrale/in/Authorizer.java
+++ src/main/java/ar/com/intrale/in/Authorizer.java
@@ -4,6 +4,7 @@ import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import com.nimbusds.jose.JOSEException;
@@ -21,7 +22,7 @@ public class Authorizer {
 	@Value("${authorizer.enabled:false}")
 	private Boolean enabled;
 	
-	@Autowired
+	@Autowired(required = false)
 	private ConfigurableJWTProcessor processor;
 	
 	@Autowired
