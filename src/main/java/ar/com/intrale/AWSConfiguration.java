83490b28a10ac32902950e99bd82f5ab91d51e19
llarreta <leonel@larreta.com.ar>
2020-05-05 16:12:10 -0300

    Bug Initialize ConfigurableJWTProcessor #8

--- src/main/java/ar/com/intrale/AWSConfiguration.java
+++ src/main/java/ar/com/intrale/AWSConfiguration.java
@@ -1,14 +1,30 @@
 package ar.com.intrale;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration(value = AWSConfiguration.NAME)
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.jwk.source.RemoteJWKSet;
+import com.nimbusds.jose.proc.JWSKeySelector;
+import com.nimbusds.jose.proc.JWSVerificationKeySelector;
+import com.nimbusds.jose.util.DefaultResourceRetriever;
+import com.nimbusds.jose.util.ResourceRetriever;
+import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+
+@Configuration
 @PropertySource("classpath:AwsCredentials.properties")
 public class AWSConfiguration {
 	
-	public static final String NAME = "AWSConfiguration";
+	private static final String COGNITO_URL_PREFIX = "https://cognito-idp.";
+	private static final String COGNITO_URL_MID = ".amazonaws.com/";
+	private static final String COGNITO_URL_SUFIX = "/.well-known/jwks.json";
 
 	@Value("${userPoolId}")
 	private String userPoolId;
@@ -18,7 +34,53 @@ public class AWSConfiguration {
 	
 	@Value("${clientId}")
 	private String clientId;
+	
+	@Value("${connectionTimeout:2000}")
+	private Integer connectionTimeout;
+	
+	@Value("${readTimeout:2000}")
+	private Integer readTimeout;
+	
+	@Bean
+	public ConfigurableJWTProcessor JWTProcessor() throws MalformedURLException {
+	
+		System.out.println("JWTProcessor:" + connectionTimeout + ", " + readTimeout);
+		ResourceRetriever resourceRetriever = new DefaultResourceRetriever(connectionTimeout, readTimeout);
+		
+		System.out.println("JWTProcessor: Build URL");
+		StringBuilder jwtUrl = new StringBuilder();
+		jwtUrl.append(COGNITO_URL_PREFIX).append(region).append(COGNITO_URL_MID).append(userPoolId).append(COGNITO_URL_SUFIX);
+		System.out.println("JWTProcessor:" + jwtUrl.toString());
+	    URL jwkSetURL= new URL(jwtUrl.toString());
+	    
+	    System.out.println("JWTProcessor: keySource");
+	    //Creates the JSON Web Key (JWK)
+	    JWKSource keySource= new RemoteJWKSet(jwkSetURL, resourceRetriever);
+	  
+	    System.out.println("JWTProcessor: keySelector");
+	    //RSASSA-PKCS-v1_5 using SHA-256 hash algorithm
+	    JWSKeySelector keySelector= new JWSVerificationKeySelector(JWSAlgorithm.RS256, keySource);
+	    
+	    System.out.println("JWTProcessor: setJWSKeySelector");
+	    ConfigurableJWTProcessor processor = new DefaultJWTProcessor();
+	    processor.setJWSKeySelector(keySelector);
+	    
+	    System.out.println("JWTProcessor: finish constructor");
+	    return processor;
+	}
 
+	public Integer getConnectionTimeout() {
+		return connectionTimeout;
+	}
+	public void setConnectionTimeout(Integer connectionTimeout) {
+		this.connectionTimeout = connectionTimeout;
+	}
+	public Integer getReadTimeout() {
+		return readTimeout;
+	}
+	public void setReadTimeout(Integer readTimeout) {
+		this.readTimeout = readTimeout;
+	}
 	public String getClientId() {
 		return clientId;
 	}
