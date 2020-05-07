816d67c142d5dd7667114f03a852781f0e9f600e
llarreta <leonel@larreta.com.ar>
2020-05-07 17:39:42 -0300

    SuccesResponse created

---
+++ src/main/java/ar/com/intrale/in/SuccessResponse.java
@@ -0,0 +1,15 @@
+package ar.com.intrale.in;
+
+public class SuccessResponse extends Response {
+
+	private Integer statusCode = 200;
+
+	public Integer getStatusCode() {
+		return statusCode;
+	}
+
+	public void setStatusCode(Integer statusCode) {
+		this.statusCode = statusCode;
+	}
+	
+}
