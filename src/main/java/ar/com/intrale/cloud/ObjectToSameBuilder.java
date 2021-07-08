bd4916286036e0b200432e81402843a3b2052e1d
llarreta <leonel@larreta.com.ar>
2021-07-08 16:18:13 -0300

    Se agregan trazas

--- src/main/java/ar/com/intrale/cloud/ObjectToSameBuilder.java
+++ src/main/java/ar/com/intrale/cloud/ObjectToSameBuilder.java
@@ -4,18 +4,25 @@ import java.util.Map;
 
 import javax.inject.Singleton;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ar.com.intrale.cloud.exceptions.FunctionException;
 
 @Singleton
 public class ObjectToSameBuilder implements BuilderForLambda<Object, Object> {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ObjectToSameBuilder.class);
+	
 	@Override
 	public Object build(Map<String, String> headers, Object source) throws FunctionException {
+		LOGGER.info("ObjectToSameBuilder build");
 		return source;
 	}
 
 	@Override
 	public Object wrapForLambda(Object target) {
+		LOGGER.info("ObjectToSameBuilder wrapForLambda");
 		return target;
 	}
 
