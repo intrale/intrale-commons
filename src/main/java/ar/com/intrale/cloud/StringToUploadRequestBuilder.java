0c57c5b1e600cc4adb5bda1399d6bb5ba4c396fd
llarreta <leonel@larreta.com.ar>
2021-07-15 13:27:42 -0300

    Version Upgrade

--- src/main/java/ar/com/intrale/cloud/StringToUploadRequestBuilder.java
+++ src/main/java/ar/com/intrale/cloud/StringToUploadRequestBuilder.java
@@ -1,8 +1,20 @@
 package ar.com.intrale.cloud;
 
+import java.util.Map;
+
 import javax.inject.Singleton;
 
+import ar.com.intrale.cloud.exceptions.FunctionException;
+
 @Singleton
-public class StringToUploadRequestBuilder extends StringToRequestBuilder<UploadRequest> {
+public class StringToUploadRequestBuilder extends StringToRequestBuilder<UploadRequest> /*implements Builder<String, UploadRequest>*/ {
+
+	/*@Override
+	public UploadRequest build(Map<String, String> headers, String source) throws FunctionException {
+		UploadRequest request = new UploadRequest();
+		request.setHeaders(headers);
+		request.setContent(source);
+		return request;
+	}*/
 
 }
