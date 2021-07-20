f4eaf5bc889270665408b419fa2b9ee56e3ec526
llarreta <leonel@larreta.com.ar>
2021-07-20 11:30:49 -0300

    Version Upgrade

--- src/main/java/ar/com/intrale/cloud/Lambda.java
+++ src/main/java/ar/com/intrale/cloud/Lambda.java
@@ -1,4 +1,7 @@
 package ar.com.intrale.cloud;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -10,6 +13,7 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.function.aws.MicronautRequestHandler;
+import io.micronaut.http.HttpMethod;
 
 @Introspected
 public class Lambda extends MicronautRequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
@@ -29,6 +33,17 @@ public class Lambda extends MicronautRequestHandler<APIGatewayProxyRequestEvent,
 	@Override
     public APIGatewayProxyResponseEvent execute(APIGatewayProxyRequestEvent request) {
 		LOGGER.info("Ejecutando lambda");
+		
+		if ("OPTIONS".equals(request.getHttpMethod())){
+			APIGatewayProxyResponseEvent responseEvent = new APIGatewayProxyResponseEvent();
+			Map<String, String> responseHeaders = new HashMap<String, String>();
+			responseHeaders.put(FunctionConst.ACCESS_CONTROL_ALLOW_ORIGIN, FunctionConst.ALL);
+			responseHeaders.put(FunctionConst.ACCESS_CONTROL_ALLOW_METHODS, FunctionConst.GET_OPTIONS_HEAD_PUT_POST);
+			responseHeaders.put(FunctionConst.ACCESS_CONTROL_ALLOW_HEADERS, FunctionConst.ALLOW_HEADERS_AVAIABLES);
+			responseEvent.setHeaders(responseHeaders); 
+	    	responseEvent.setIsBase64Encoded(Boolean.TRUE);
+	    	return responseEvent;
+		}
 		
 		function = builder.getfunction(request.getHeaders());
 		
